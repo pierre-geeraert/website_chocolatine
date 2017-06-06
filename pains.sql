e8c35f05f5583a47a6ac70cd3c1eac2ec89a2847
pierre-geeraert <pierre.geeraert@viacesi.fr>
2017-06-06 10:20:28 +0200

    Update pains.sql

--- pains.sql
+++ pains.sql
@@ -5,11 +5,11 @@ CREATE TABLE pains (
         comment varchar(70) default 'pas de commentaire'
         );
         
-        
-insert into pains values(1,'2017-04-28',49,'pas de commentaire');
-insert into pains values(2,'2017-05-05',45,'diminue car paiement boissons petit dej journée de selection');
-insert into pains values(3,'2017-05-12',63,'perte de 30 euros pour selection avec ajout de 50 aprés');
-insert into pains values(4,'2017-05-15',65.5,'pas de commentaire');
-insert into pains values(5,'2017-05-23',64,'diminution car changement prix dù aux fournisseurs');
-insert into pains values(6,'2017-06-01',60.15,'pas de commentaire');
-insert into pains values(7,'2017-06-02',65.7,'pas de commentaire');
+      
+insert into pains(horodateur,argent,comment) values('2017-04-28',49,'pas de commentaire');
+insert into pains(horodateur,argent,comment) values('2017-05-05',45,'diminue car paiement boissons petit dej journée de selection');
+insert into pains(horodateur,argent,comment) values('2017-05-12',63,'perte de 30 euros pour selection avec ajout de 50 aprés');
+insert into pains(horodateur,argent,comment) values('2017-05-15',65.5,'pas de commentaire');
+insert into pains(horodateur,argent,comment) values('2017-05-23',64,'diminution car changement prix dù aux fournisseurs');
+insert into pains(horodateur,argent,comment) values('2017-06-01',60.15,'pas de commentaire');
+insert into pains(horodateur,argent,comment) values('2017-06-02',65.7,'pas de commentaire');
