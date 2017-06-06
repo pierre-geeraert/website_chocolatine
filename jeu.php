7b403027f3738496cb78e661837c38f9bff874ae
pierre-geeraert <pierre.geeraert@viacesi.fr>
2017-06-06 23:20:24 +0200

    Create jeu.php

---
+++ jeu.php
@@ -0,0 +1,6 @@
+<audio src="cena.mp3"  autoplay></audio>
+					<br />
+					<script language="javascript">
+						alert("Felicitation c'est bon");
+					</script>
+<meta http-equiv="refresh" content="0; url=form_pains.php" />
